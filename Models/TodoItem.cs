1feacda88b4e7a7f00bb8fbcac82ac98458e8e96
Muhammad Ammar (ammarm2) <ammarm2@coventry.ac.uk>
2024-03-22 10:06:16 +0000

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+        public bool IsPinned { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public bool IsImportant { get; set; }
+        public TodoItem()
+        {
+            Deadline = DateTime.Today;
+            string dateOnlyString = Deadline.ToString("yyyy-MM-dd"); // Format to display date only
+            Console.WriteLine("Date only: " + dateOnlyString);
+        }
+    }
+}
